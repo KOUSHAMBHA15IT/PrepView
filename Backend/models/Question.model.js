bbee62b4cdff27057fa8829ab32fa8c8520343c2
SubhradeepBasu18 <subhradeepbasu2002@gmail.com>
2025-07-31 20:41:40 +0530

    add auth features

---
+++ Backend/models/Question.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const questionSchema = new mongoose.Schema({
+    session: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Session',
+    },
+    question: {
+        type: String,
+    },
+    answer: {
+        type: String,
+    },
+    isPinned: {
+        type: Boolean,
+        default: false,
+    }
+}, {timestamps: true});
+
+export const Question = new mongoose.model('Question', questionSchema);
